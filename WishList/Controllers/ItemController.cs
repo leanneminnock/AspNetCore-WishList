1e05584a3521f9c3e89d5e570b9ec52ef8bea143
leanneminnock <lelannem91@gmail.com>
2021-04-09 17:17:17 +0100

    ItemController.cs setup
    - inject DbContext to ItemController.cs
    - create get and post methods on Item controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Item/Index");
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View("Item/Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Item/Index");
+        }
+    }
+}
